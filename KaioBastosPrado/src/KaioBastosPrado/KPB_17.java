08bb9edd2371e339c6808ce14f18633573b35c93
Kaio Bastos Prado <89665041+KaioBr01@users.noreply.github.com>
2025-11-19 11:18:12 -0300

    Add files via upload

--- KaioBastosPrado/src/KaioBastosPrado/KPB_17.java
+++ KaioBastosPrado/src/KaioBastosPrado/KPB_17.java
@@ -4,7 +4,7 @@ public class KPB_17 {
 
     public static void main(String[] args) {
 
-        System.out.println("Aluno: Kaio Bastos – RA: ______");
+        System.out.println("Aluno: Kaio Bastos – RA: 0023338");
         System.out.println("Classe KBP17 – Quadrado de 1 a 10");
         System.out.println("---------------------------------------------");
 
